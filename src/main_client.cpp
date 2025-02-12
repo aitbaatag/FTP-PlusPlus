209d5b814dc1143e65f601c68435d48abd7c4b95
aitbaatag <aitbaatagkhalid@gmail.com>
2025-02-12 15:59:02 +0100

    Update

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -1,14 +1,13 @@
 #include "../inc/Client/Client.hpp"
 #include <iostream>
 
-int main()
-{
+int main() {
   char *ip;
 
-  ip= "127.0.0.1";
-    Client client("127.0.0.1", 2000);
+  ip = "127.0.0.1";
+  Client client("127.0.0.1", 2000);
 
-    client.CreateTCPIpv4Socket();
-    client.InitSocketAdd();
-    client.ConnectToServer();
+  client.CreateTCPIpv4Socket();
+  client.InitSocketAdd();
+  client.ConnectToServer();
 }
